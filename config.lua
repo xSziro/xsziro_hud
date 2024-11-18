d84eec359c9b6f5f1fee27eea00ec9a37c88b730
xSziro <57913062+xSziro@users.noreply.github.com>
2024-11-18 05:55:40 +0100

    removed locales from config(using ox_lib locales)

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.Locale = 'en'
+
 Config.metrics = 'kph' -- or mph
 Config.seatbelt = true -- seatbelt icon
 Config.seatbeltgetter = function () -- seatbelt state getter function
